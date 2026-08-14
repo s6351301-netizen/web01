4744fee8491593eee9d9b3e324d3ba1187ded786
may <s6351301@gmail.com>
2026-08-14 10:27:30 +0800

    增加管理登入的判斷及後台的基本防護

--- admin.php
+++ admin.php
@@ -1,4 +1,11 @@
-﻿<?php include_once "./api/db.php";?>
+﻿<?php include_once "./api/db.php";
+if(!isset($_SESSION['login'])){
+	echo "非法登入,請回首頁重新登入";
+	echo "<a href='../index.php?do=login'>管理登入</a>";
+	exit();
+}
+
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0068)?do=admin&redo=title -->
 <html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
---
+++ api/logout.php
@@ -0,0 +1,5 @@
+<?php 
+include_once "db.php";
+unset($_SESSION['login']);
+to("../index.php?do=login");
+?>
